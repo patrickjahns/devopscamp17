ee8c5183693f7a2d28bfe350a48b08243b8f6e66
Patrick Jahns <github@patrickjahns.de>
2017-06-01 11:40:36 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo "Connection failed: " . $conn->connect_error;
     exit(1);
 }
 echo "Connected successfully";
